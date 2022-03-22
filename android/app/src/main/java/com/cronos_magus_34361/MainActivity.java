d30e0ab28cef643cd0d14a3d771ee739f7f99dac
Crowdbotics <team@crowdbotics.com>
2022-03-22 05:45:55 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/cronos_magus_34361/MainActivity.java
@@ -0,0 +1,15 @@
+package com.cronos_magus_34361;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "cronos_magus_34361";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "cronos_magus_34361",
+  url: "https://cronos_magus_34361.botics.co",
+  api: "https://cronos_magus_34361.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
